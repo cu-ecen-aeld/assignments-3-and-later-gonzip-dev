8f167142b114b9e742eed1cb3c668eeab6f44e5c
arturo <arturo@Ubuntu.myguest.virtualbox.org>
2026-06-15 21:49:32 +0200

    Changed and cleaned code. Changed type of copying.

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -6,16 +6,13 @@ set -e
 set -u
 
 OUTDIR=/tmp/aeld
-BASE_DIR=$(pwd)
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
-LIB_DEPENDANCIES_DIR=/usr/local/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu
-
-export PATH=$PATH:/usr/local/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
 
 if [ $# -lt 1 ]
 then
@@ -38,9 +35,11 @@ mkdir -p home/conf
 
 # TODO: Add library dependencies to rootfs
 
-cp $BASE_DIR/ld-linux-aarch64.so.1 $OUTDIR/rootfs/lib
+SYSROOT_DIR=$(${CROSS_COMPILE}gcc -print-sysroot)
 
-cp $BASE_DIR/finder-app/{libm.so.6,libresolv.so.2,libc.so.6} $OUTDIR/rootfs/lib64
+cp $SYSROOT_DIR/lib/ld-linux-aarch64.so.1 $OUTDIR/rootfs/lib
+
+cp $SYSROOT_DIR/lib64/{libm.so.6,libresolv.so.2,libc.so.6} $OUTDIR/rootfs/lib64
 
 
 cd "$OUTDIR"
@@ -108,9 +107,6 @@ echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a $OUTDIR/rootfs/bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a $OUTDIR/rootfs/bin/busybox | grep "Shared library"
 
-
-
-
 # TODO: Make device nodes
 
 sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
@@ -119,19 +115,19 @@ sudo mknod -m 666 ${OUTDIR}/rootfs/dev/console c 5 1
 # TODO: Clean and build the writer utility
 echo "clean and build"
 
-cd "$BASE_DIR/finder-app"
+cd "$FINDER_APP_DIR"
 make clean
 make CROSS_COMPILE=${CROSS_COMPILE} all
 
 # move writer application
 echo "move writer"
-cp $BASE_DIR/finder-app/writer "$OUTDIR/rootfs/home"
+cp $FINDER_APP_DIR/writer "$OUTDIR/rootfs/home"
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
 echo "move scripts and execs"
-cp $BASE_DIR/finder-app/{finder.sh,finder-test.sh,autorun-qemu.sh} $OUTDIR/rootfs/home
-cp $BASE_DIR/conf/{username.txt,assignment.txt} $OUTDIR/rootfs/home/conf
+cp $FINDER_APP_DIR/{finder.sh,finder-test.sh,autorun-qemu.sh} $OUTDIR/rootfs/home
+cp $FINDER_APP_DIR/{username.txt,assignment.txt} $OUTDIR/rootfs/home/conf
 
 # TODO: Chown the root directory
 sudo chown 777 $OUTDIR/rootfs
